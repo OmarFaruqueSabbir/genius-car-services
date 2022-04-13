636b77ec6a64e80e1899951b64b790528cd658e5
OmarFaruqueSabbir <avrosabbir18@gmail.com>
2022-04-13 16:46:58 +0600

    Checkout in RequireAuth, !user => Login before going to checkout page, if login => signout button will popup in navbar/header

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import { Container, Nav, Navbar,NavDropdown } from 'react-bootstrap';
+import { signOut } from 'firebase/auth';
+import React, { useState } from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import logo from '../../../images/logo.png'
 
 const Header = () => {
+    const [user] = useAuthState(auth);
+    const handleSignOut = () =>{
+        signOut(auth);
+    }
     return (
         <header>
             <>
-                <Navbar collapseOnSelect expand="lg" sticky='top'  bg="dark" variant="dark">
+                <Navbar collapseOnSelect expand="lg" sticky='top' bg="dark" variant="dark">
                     <Container>
-                        <Navbar.Brand as ={Link} to="/">
-                            <img src= {logo} height={30} alt="" />
+                        <Navbar.Brand as={Link} to="/">
+                            <img src={logo} height={30} alt="" />
                         </Navbar.Brand>
                         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                         <Navbar.Collapse id="responsive-navbar-nav">
@@ -27,9 +34,14 @@ const Header = () => {
                             </Nav>
                             <Nav>
                                 <Nav.Link as={Link} to="/about">About</Nav.Link>
-                                <Nav.Link as={Link} to="/login">
-                                   Login
-                                </Nav.Link>
+                                {
+                                    user?
+                                    <button className='btn btn-primary' onClick={handleSignOut}>signOut</button>
+                                    :
+                                    <Nav.Link as={Link} to="/login">
+                                        Login
+                                    </Nav.Link>
+                                }
                             </Nav>
                         </Navbar.Collapse>
                     </Container>
